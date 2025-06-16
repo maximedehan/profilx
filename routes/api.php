34754c9e82207d39a42a8afa0cbe38fc4c91e4c9
maximedehan <maximedehan@aol.com>
2025-06-16 21:56:58 +0200

    adding of routes API

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdministrateurController;
+use App\Http\Controllers\ProfilController;
+use App\Http\Controllers\CommentaireController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +16,9 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::apiResource('administrateurs', AdministrateurController::class);
+Route::apiResource('profils', ProfilController::class);
+Route::apiResource('commentaires', CommentaireController::class);
